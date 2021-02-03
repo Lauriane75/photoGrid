fccc9ed5e6a1a04f5e052c1c47486fe77adfdde0
Lauriane75 <lauriane.haydari@gmail.com>
2021-02-03 10:35:11 +0100

    Create project

---
+++ photoGrid/photoGrid/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  photoGrid
+//
+//  Created by Lauriane Haydari on 03/02/2021.
+//  Copyright © 2021 Lauriane Haydari. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        let rootViewController = ViewController(nibName: nil, bundle: nil)
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
+
